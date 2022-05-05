88ee8217058dfc6452b6c7fc8cd90235eb7aae93
SnehalKawade <104971979+SnehalKawade@users.noreply.github.com>
2022-05-05 20:03:32 +0530

    Add files via upload

---
+++ Home_Work/CourseListDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work
+{
+    class CourseList
+    {
+        public int CId { get; set; }
+        public string CName { get; set; }
+
+        public int CourseFee { get; set; }
+    }
+    class CourseListDemo
+    {
+        static void Main(string[] args)
+        {
+            List<CourseList> clist=new List<CourseList>()
+            {
+                new CourseList{CId= 10001, CName= "JAVA", CourseFee= 45000},
+                new CourseList{CId= 10002, CName= "C# ", CourseFee= 40000},
+                new CourseList{CId= 10003, CName= "PHP", CourseFee= 35000},
+                new CourseList{CId= 10004, CName= "C  ", CourseFee= 10000},
+                new CourseList{CId= 10005, CName= "C++", CourseFee= 20000},
+                new CourseList{CId= 10006, CName= "PYTHON", CourseFee= 38000}
+            };
+
+            foreach(CourseList c in clist)
+            {
+                Console.WriteLine($"CourseId: {c.CId}\t CourseName: {c.CName}\t CourseFees: {c.CourseFee}");
+            }
+        }
+    }
+}
---
+++ Home_Work/DepartmentListDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work
+{
+    class DepartmentList
+    {
+        public int DId { get; set; }    
+        public string DName { get; set; }   
+
+    }
+    class DepartmentListDemo
+    {
+        static void Main(string[] args)
+        {
+            List<DepartmentList> departlist = new List<DepartmentList>()
+            {
+                new DepartmentList{DId =11111, DName= "Snehal"},
+                new DepartmentList{DId =22222, DName= "Shravy"},
+                new DepartmentList{DId =33333, DName= "Prasad"},
+                new DepartmentList{DId =44444, DName= "Sayali"},
+                new DepartmentList{DId =11111, DName= "Snehal"},
+            };
+            foreach(DepartmentList d in departlist)
+            {
+                Console.WriteLine($"Department Id: {d.DName}\t Department Name: {d.DName}");
+            }
+        }
+    }
+}
